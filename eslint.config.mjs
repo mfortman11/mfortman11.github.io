59097ede071a3406d55dfa23ad116b3349c3a322
Mike <mfortman11@gmail.com>
2026-08-11 12:27:57 -0500

    style fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,18 @@
 import eslint from '@eslint/js';
 import astro from 'eslint-plugin-astro';
+import tsParser from '@typescript-eslint/parser';
 
 export default [
   { ignores: ['dist/**', '.astro/**', 'node_modules/**', 'public/archive/**'] },
   eslint.configs.recommended,
   ...astro.configs.recommended,
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: ['.astro'],
+      },
+    },
+  },
 ];
